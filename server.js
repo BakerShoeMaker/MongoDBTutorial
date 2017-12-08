80c4a09d4942ce02a62d1deab6475f7355a91ba0
BakerShoeMaker <info@quackenworth.com>
2017-12-08 12:12:47 -0800

    having problem connecting to database.

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const MongoClient = require('mongodb').MongoClient;
+var db;
+
+const uri = "mongodb://dbAdminBakerShoeMaker:6352641Football@ds133796.mlab.com:33796/tutuorialtest_db";
+
+MongoClient.connect(uri, (err, database) => {
+    if(err) return console.log("There is problem: ", err);
+    db = database;
+
+    app.listen(port, ()=> {
+        console.log('listening on: ' +port);
+    });
+});
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -9,11 +23,13 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 
 });
-app.post('/quotes', (req, res)=>{
 
-    console.log("You clicked on quotes!!!!");
+app.post('/quotes', (req, res)=>{
+    db.collection('quotes').save(req.body, (err,result) =>{
+        if(err) return console.log(err);
+        console.log('saved to database');
+        res.redirect('/');
+
+    });
 });
 
-app.listen(port, ()=> {
-    console.log('listening on: ' +port);
-})
